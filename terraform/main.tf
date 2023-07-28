bca0bae9c07944edb5e6683b32c2ca41da613a25
xueqiut <tianjian0809@gmail.com>
2023-07-28 11:33:41 -0400

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.10.0"
+    }
+  }
+}
+
+backend "s3" {
+  bucket         = "terraform-state-management-axeisie7ze"
+  key            = "learn-terraform-module-object-attributes-s3/terraform.tfstate"
+  region         = "us-east-1"
+  encrypt        = true
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.nano"
+  tags = {
+    Name = "test_instance"
+  }
+}
